8b3cf66e7f6fe06a4748c031f6c3ed0ea4f75a01
Sumit Gouthaman <sumitgt007@gmail.com>
2015-05-15 18:53:32 -0700

    Added ChronoSync (Everyone can make changes). Random name at startup.

--- app/src/main/java/edu/ucla/cs/ndnwhiteboard/Utils.java
+++ app/src/main/java/edu/ucla/cs/ndnwhiteboard/Utils.java
@@ -13,4 +13,15 @@ public class Utils {
         int minute = Calendar.getInstance().getTime().getMinutes()/10;
         return "board_" + timeStamp + "" + minute;
     }
+
+    public static String generateRandomName() {
+        String seed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int length = 6;
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<length;i++){
+            int random = (int)(Math.random()*seed.length());
+            sb.append(seed.charAt(random)+"");
+        }
+        return sb.toString();
+    }
 }
